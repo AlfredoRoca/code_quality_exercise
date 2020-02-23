9661a2e79ac07de72a34af69c209b71f50ba3309
Alfredo <alfredo.roca.mas@gmail.com>
2020-02-23 01:17:23 +0100

    refactorizes parameters validation service

--- src/clean_code/parameters_validation_service.rb
+++ src/clean_code/parameters_validation_service.rb
@@ -1,16 +1,66 @@
 class ParametersValidationService
   REQUIRED_PARAMETERS = %i[side amount currency counter_currency date price order_id]
+  NUMERIC_FIELDS = %i[amount price]
+  POSITIVE_FIELDS = %i[amount price]
+  STRING_FIELDS = %i[side currency counter_currency order_id]
+  DATE_FIELDS = %i[date]
 
   def initialize(**params)
     @params = params.compact
   end
 
   def valid?
-    REQUIRED_PARAMETERS.all? { |param| @params.include?(param) && !@params[param].empty? }
+    REQUIRED_PARAMETERS.all? do |param|
+      required_param_is_present?(param) &&
+      param_is_not_nil?(@params[param]) &&
+      string_param_not_empty?(param) &&
+      numeric_field_is_a_number?(param) &&
+      numeric_positive_field_is_positive?(param) &&
+      param_date_is_kind_of_a_date_in_the_future?(param)
+    end
+  end
+
+  def required_param_is_present?(key)
+    @params.include?(key)
+  end
+
+  def param_is_not_nil?(value)
+    !value.nil?
+  end
+
+  def string_param_not_empty?(param)
+    (!STRING_FIELDS.include?(param) || !@params[param].empty?)
+  end
+
+  def numeric_field_is_a_number?(param)
+    (!NUMERIC_FIELDS.include?(param) || param_numeric_is_number?(@params[param]))
+  end
+
+  def param_numeric_is_number?(value)
+    value = value.to_f if value.class == String && value =~ /^[+|-]?\d?\.?\d+$/
+    [Integer, Float].include?(value.class)
+  end
+
+  def numeric_positive_field_is_positive?(param)
+    (!POSITIVE_FIELDS.include?(param) || number_is_positive?(@params[param]))
+  end
+
+  def number_is_positive?(value)
+    value = value.to_f if value.class == String && value =~ /^[+|-]?\d?\.?\d+$/
+    value.positive?
+  end
+
+  def param_date_is_kind_of_a_date_in_the_future?(param)
+    (!DATE_FIELDS.include?(param) || param_date_is_in_the_future?(@params[param]))
+  end
+
+  def param_date_is_in_the_future?(value)
+    Date.parse(value) > Date.today
+  rescue => e
+    false
   end
 
   def invalid?
     !valid?
   end
-
 end
--- src/clean_code/spec/parameters_validation_service_spec.rb
+++ src/clean_code/spec/parameters_validation_service_spec.rb
@@ -1,47 +1,111 @@
 require_relative '../parameters_validation_service.rb'
+require 'date'
+
+RSpec.shared_examples :valid_parameters do
+  it 'returns true' do
+    expect(described_class.new(params).valid?).to be true
+  end
+end
+
+RSpec.shared_examples :invalid_parameters do |param, new_value|
+  it 'returns false' do
+    params[param] = new_value if param
+
+    expect(described_class.new(params).valid?).to be false
+  end
+end
 
 RSpec.describe ParametersValidationService do
+
+  describe '#param_numeric_is_number?(value)' do
+    it 'returns true when value is kind of a number' do
+      [-1, 0, 1, '123', '-1.5', '1.5'].each do |number|
+        expect(described_class.new.param_numeric_is_number?(number)).to be true
+      end
+    end
+
+    it 'returns false when value is a string containing not a number' do
+      %w[qwe 23w 34qw45 po212].each do |string|
+        expect(described_class.new.param_numeric_is_number?(string)).to be false
+      end
+    end
+  end
+
+  describe '#param_date_is_in_the_future?(value)' do
+    it 'returns true when value is kind of a date in the future' do
+      ['2/1/2030', '2030-07-25'].each do |date|
+        expect(described_class.new.param_date_is_in_the_future?(date)).to be true
+      end
+    end
+
+    it 'returns false when value is kind of a date in the past' do
+      ['2/1/2020', '2019-07-25'].each do |date|
+        expect(described_class.new.param_date_is_in_the_future?(date)).to be false
+      end
+    end
+  end
+
   describe '#valid?' do
+    let(:params) {
+      {
+        side: 'buy',
+        amount: 1_000_000,
+        currency: 'USD',
+        counter_currency: 'EUR',
+        date: (Date.today + 5).to_s,
+        price: 1.1345,
+        order_id: 'X-A213FFL'
+      }
+    }
 
-    shared_examples_for :valid_parameters do |params|
-      it 'returns true if all required parameters are present and not empty' do
-        expect(described_class.new(params).valid?).to be true
-      end
-    end
-
-    shared_examples_for :invalid_parameters do |params|
-      it 'returns false' do
-        expect(described_class.new(params).valid?).to be false
-      end
-    end
-
-    context do
-      full_params = {}
-      described_class::REQUIRED_PARAMETERS.each { |param| full_params.merge!({ "#{param}": param.to_s }) }
-
-      it_behaves_like :valid_parameters, full_params, "eooo"
+    context 'with good params, order executable' do
+      it_behaves_like :valid_parameters
     end
 
     context do
       context 'if some missing parameter' do
-        params = {}
-        [:amount].each { |param| params.merge!({ "#{param}": param.to_s }) }
+        let(:params) {
+          {
+            amount: 1_000_000,
+            currency: 'USD',
+            counter_currency: 'EUR',
+            date: (Date.today + 5).to_s,
+            price: 1.1345,
+            order_id: 'X-A213FFL'
+          }
+        }
 
-        it_behaves_like :invalid_parameters, params
+        it_behaves_like :invalid_parameters
       end
 
       context 'if some parameter is nil' do
-        params = {}
-        described_class::REQUIRED_PARAMETERS.each { |param| params.merge!({ "#{param}": nil }) }
-
-        it_behaves_like :invalid_parameters, params
+        it_behaves_like :invalid_parameters, :currency, nil
       end
 
       context 'if some parameter is an empty string' do
-        params = {}
-        described_class::REQUIRED_PARAMETERS.each { |param| params.merge!({ "#{param}": '' }) }
+        it_behaves_like :invalid_parameters, :order_id, ''
+      end
 
-        it_behaves_like :invalid_parameters, params
+      context 'if value date is in the past' do
+        it_behaves_like :invalid_parameters, :date, '1970-01-01'
+      end
+
+      context 'if amount is negative or 0' do
+        it_behaves_like :invalid_parameters, :amount, -1
+        it_behaves_like :invalid_parameters, :amount, 0
+      end
+
+      context 'if amount is not a number' do
+        it_behaves_like :invalid_parameters, :amount, 'qwe'
+      end
+
+      context 'if exchange rate is negative or 0' do
+        it_behaves_like :invalid_parameters, :price, -1
+        it_behaves_like :invalid_parameters, :price, 0
+      end
+
+      context 'if exchange rate is not a number' do
+        it_behaves_like :invalid_parameters, :price, 'qwe'
       end
     end
   end
